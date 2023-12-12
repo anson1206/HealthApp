5b8cd809760adb31ce44badfdb4c37f5ade0cd8a
anson12 <anson15205@gmail.com>
2023-12-12 14:20:01 -0500

    Shows heart rate, distance, and flights of stairs climbed. Would like to be able to get a button that will display a data frame under the charts

--- mainHealth.py
+++ mainHealth.py
@@ -6,9 +6,8 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('export.xml')
 root = tree.getroot()
 
-# Parsing heart rate data
+# Goes through the data from Apple Health and shows the heart rate over time
 heart_rate_data = []
-
 for record in root.findall('.//Record[@type="HKQuantityTypeIdentifierHeartRateVariabilitySDNN"]'):
     creation_date_str = record.get('creationDate')
     creation_date = datetime.strptime(creation_date_str, "%Y-%m-%d %H:%M:%S %z")
@@ -26,23 +25,18 @@ for record in root.findall('.//Record[@type="HKQuantityTypeIdentifierHeartRateVa
 
 heart_rate_df = pd.DataFrame(heart_rate_data)
 
-# Parsing workout data
+#Goes through workouts set by the user and shows the distance the user had gone in miles
 workout_info = []
-
-
-
 for workout in root.findall('.//Workout'):
     distance = float('nan')
     start_date_str = workout.get('startDate')
     start_date = datetime.strptime(start_date_str, "%Y-%m-%d %H:%M:%S %z")
-    print(start_date)
     unit = workout.get('unit')
     workout_type = workout.get('workoutActivityType')
-    print(workout_type)
     for dist in workout.findall('WorkoutStatistics'):
         if dist.get('type') == 'HKQuantityTypeIdentifierDistanceWalkingRunning':
             distance = float(dist.get('sum'))
-            print(distance)
+    # If there isn't a distance, sets it to 0
     if pd.isna(distance):
         distance = 0.0
     workout_info.append({
@@ -55,8 +49,8 @@ workoutNew_df = pd.DataFrame(workout_info)
 
 
 
-# Mapping of activity types to more descriptive names
-activity_mapping = {
+# A dictionary for the activities. Makes it easier to read the activities
+activity_dictionary = {
     'HKWorkoutActivityTypeBaseball': 'Baseball',
     'HKWorkoutActivityTypeRunning': 'Running',
     'HKWorkoutActivityTypeWalking': 'Walking',
@@ -65,13 +59,11 @@ activity_mapping = {
 
 }
 
-# Replace activity types with descriptive names
-workoutNew_df['WorkoutType'] = workoutNew_df['Workout'].map(activity_mapping)
+# Uses the activity_dictionary to replace the Apple Health names
+workoutNew_df['WorkoutType'] = workoutNew_df['Workout'].map(activity_dictionary)
 
-# Convert 'Date' column to datetime in heart_rate_df
+# Convert 'Date' column to datetime in heart_rate_df and workoutNew_df
 heart_rate_df['Date'] = pd.to_datetime(heart_rate_df['Date']).dt.date
-
-# Convert 'Date' column to datetime in workoutNew_df
 workoutNew_df['Date'] = pd.to_datetime(workoutNew_df['Date']).dt.date
 
 # Merge heart rate and workout data on Date
@@ -81,29 +73,68 @@ merged_df = pd.merge(heart_rate_df, workoutNew_df, on='Date', how='left')
 
 
 
-# Convert time to string in 12-hour format from the heart rate df
+# Converts time to string in 12-hour format from the heart rate df
 merged_df['Time'] = merged_df['Time'].apply(lambda x: x.strftime("%I:%M:%S %p"))
 
 # Group by Date and take the maximum Distance for each day
 distance_per_day = merged_df[merged_df['WorkoutType'].isin(['Running', 'Walking', 'RunningWalking', 'Baseball'])].groupby('Date')['Distance'].max().reset_index()
 
+
+
+
+
+
+
+#Shows the number of flight of stairs you went up
+flights_climbed_data = []
+for record in root.findall('.//Record[@type="HKQuantityTypeIdentifierFlightsClimbed"]'):
+    creation_date_str = record.get('creationDate')
+    creation_date = datetime.strptime(creation_date_str, "%Y-%m-%d %H:%M:%S %z")
+    flight_count = int(record.get('value'))
+    print(creation_date)
+    print(flight_count)
+
+    flights_climbed_data.append({
+        'Date': creation_date.date(),
+        'Flights': flight_count
+    })
+stair_count_df = pd.DataFrame(flights_climbed_data)
+
+#Counts the total number of stairs
+total_flights = 0
+for stairs in stair_count_df['Flights']:
+    total_flights+= flight_count
+
+print(total_flights)
+
+
+
 # Streamlit App
 st.title("Health Data Explorer")
 
 # Sidebar for date and data selection
-selected_date = st.sidebar.date_input("Select Date", min_value=min(heart_rate_df['Date']),
-                                      max_value=max(heart_rate_df['Date']), value=min(heart_rate_df['Date']))
+#selected_date = st.sidebar.date_input("Select Date", min_value=min(heart_rate_df['Date']),
+                                     #max_value=max(heart_rate_df['Date']), value=min(heart_rate_df['Date']))
 
-selected_data = st.sidebar.radio("Select Data to Display", ["Heart Rate", "Distance"])
+# The side bar for the user to select things
+selected_data = st.sidebar.radio("Select Data to Display", ["Heart Rate", "Distance", "Flights Climbed"])
 
 # Filter data for the selected date
-filtered_df = merged_df[merged_df['Date'] == selected_date]
+#filtered_df = merged_df[merged_df['Date'] == selected_date]
+
 
 # Plot the selected data over time
 if selected_data == "Heart Rate":
+    selected_date = st.sidebar.date_input("Select Date", min_value=min(heart_rate_df['Date']), max_value=max(heart_rate_df['Date']), value=min(heart_rate_df['Date']))
+    filtered_df = merged_df[merged_df['Date'] == selected_date]
+
+    st.write('Here is your heart rate over time')
     st.line_chart(filtered_df.set_index('Time')['HeartRate'])
 elif selected_data == "Distance":
     st.bar_chart(distance_per_day.set_index('Date')['Distance'])
+elif selected_data == "Flights Climbed":
+    st.write('You Climbed ' , total_flights , ' flights of stairs')
+    st.line_chart(stair_count_df.set_index('Date')['Flights'])
 
 # Display the DataFrame
-st.dataframe(filtered_df[['Date', 'Time', 'HeartRate', 'Distance']])
+#st.dataframe(filtered_df[['Date', 'Time', 'HeartRate', 'Distance']])
