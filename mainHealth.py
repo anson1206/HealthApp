42edac282a8768f36bb0ed4a03156d032ac758fa
anson12 <anson15205@gmail.com>
2023-12-12 21:15:01 -0500

    Shows bpm, distance, flights climbed, and data frame

--- mainHealth.py
+++ mainHealth.py
@@ -11,12 +11,10 @@ heart_rate_data = []
 for record in root.findall('.//Record[@type="HKQuantityTypeIdentifierHeartRateVariabilitySDNN"]'):
     creation_date_str = record.get('creationDate')
     creation_date = datetime.strptime(creation_date_str, "%Y-%m-%d %H:%M:%S %z")
-
     for entry in record.findall('.//InstantaneousBeatsPerMinute'):
         time_str = entry.get('time')
         time = datetime.strptime(time_str, "%I:%M:%S.%f %p").time()
         heart_rate = int(entry.get('bpm'))
-
         heart_rate_data.append({
             'Date': creation_date.date(),
             'Time': time,
@@ -62,16 +60,32 @@ activity_dictionary = {
 # Uses the activity_dictionary to replace the Apple Health names
 workoutNew_df['WorkoutType'] = workoutNew_df['Workout'].map(activity_dictionary)
 
-# Convert 'Date' column to datetime in heart_rate_df and workoutNew_df
+# Converts the 'Date' column to datetime in heart_rate_df and workoutNew_df
 heart_rate_df['Date'] = pd.to_datetime(heart_rate_df['Date']).dt.date
 workoutNew_df['Date'] = pd.to_datetime(workoutNew_df['Date']).dt.date
 
-# Merge heart rate and workout data on Date
-merged_df = pd.merge(heart_rate_df, workoutNew_df, on='Date', how='left')
+
+#Shows the number of flight of stairs you went up
+flights_climbed_data = []
+for record in root.findall('.//Record[@type="HKQuantityTypeIdentifierFlightsClimbed"]'):
+    creation_date_str = record.get('creationDate')
+    creation_date = datetime.strptime(creation_date_str, "%Y-%m-%d %H:%M:%S %z")
+    flight_count = int(record.get('value'))
+    flights_climbed_data.append({
+        'Date': creation_date.date(),
+        'Flights': flight_count
+    })
+stair_count_df = pd.DataFrame(flights_climbed_data)
+
+#Counts the total number of flights
+total_flights = 0
+for stairs in stair_count_df['Flights']:
+    total_flights+= flight_count
 
 
 
-
+#merges thr data frames together on Date
+merged_df = pd.merge(pd.merge(heart_rate_df, workoutNew_df, on='Date', how='left'), stair_count_df, on='Date', how='left')
 
 # Converts time to string in 12-hour format from the heart rate df
 merged_df['Time'] = merged_df['Time'].apply(lambda x: x.strftime("%I:%M:%S %p"))
@@ -81,60 +95,33 @@ distance_per_day = merged_df[merged_df['WorkoutType'].isin(['Running', 'Walking'
 
 
 
-
-
-
-
-#Shows the number of flight of stairs you went up
-flights_climbed_data = []
-for record in root.findall('.//Record[@type="HKQuantityTypeIdentifierFlightsClimbed"]'):
-    creation_date_str = record.get('creationDate')
-    creation_date = datetime.strptime(creation_date_str, "%Y-%m-%d %H:%M:%S %z")
-    flight_count = int(record.get('value'))
-    print(creation_date)
-    print(flight_count)
-
-    flights_climbed_data.append({
-        'Date': creation_date.date(),
-        'Flights': flight_count
-    })
-stair_count_df = pd.DataFrame(flights_climbed_data)
-
-#Counts the total number of stairs
-total_flights = 0
-for stairs in stair_count_df['Flights']:
-    total_flights+= flight_count
-
-print(total_flights)
-
-
-
 # Streamlit App
 st.title("Health Data Explorer")
 
-# Sidebar for date and data selection
-#selected_date = st.sidebar.date_input("Select Date", min_value=min(heart_rate_df['Date']),
-                                     #max_value=max(heart_rate_df['Date']), value=min(heart_rate_df['Date']))
-
 # The side bar for the user to select things
-selected_data = st.sidebar.radio("Select Data to Display", ["Heart Rate", "Distance", "Flights Climbed"])
+selected_data = st.sidebar.radio("Select Data to Display", ["Heart Rate", "Distance", "Flights Climbed", "Data Frame"])
+
+selected_date = st.sidebar.date_input("Select Date", min_value=min(merged_df['Date']), max_value=max(merged_df['Date']), value=min(merged_df['Date']))
 
 # Filter data for the selected date
-#filtered_df = merged_df[merged_df['Date'] == selected_date]
+filtered_df = merged_df[merged_df['Date'] == selected_date]
+
+
 
 
 # Plot the selected data over time
 if selected_data == "Heart Rate":
-    selected_date = st.sidebar.date_input("Select Date", min_value=min(heart_rate_df['Date']), max_value=max(heart_rate_df['Date']), value=min(heart_rate_df['Date']))
-    filtered_df = merged_df[merged_df['Date'] == selected_date]
-
     st.write('Here is your heart rate over time')
     st.line_chart(filtered_df.set_index('Time')['HeartRate'])
+    st.dataframe(filtered_df[['Time', 'HeartRate']])
 elif selected_data == "Distance":
+    st.write('Here is the total distance over time')
     st.bar_chart(distance_per_day.set_index('Date')['Distance'])
 elif selected_data == "Flights Climbed":
     st.write('You Climbed ' , total_flights , ' flights of stairs')
     st.line_chart(stair_count_df.set_index('Date')['Flights'])
+elif selected_data == "Data Frame":
+    st.write('Here is the data frame where you can look through the data to see information')
+    st.dataframe(filtered_df[['Date', 'Time', 'HeartRate', 'Distance', 'Flights']])
 
-# Display the DataFrame
-#st.dataframe(filtered_df[['Date', 'Time', 'HeartRate', 'Distance']])
+
